801a5e48ceac450bbd0c7935806bf67b7412006e
dustycelary <fungussh@gmail.com>
2026-07-09 23:24:30 +0100

    feat: add github actions ci pipeling and ruff
    - configure ruff rules and line formatting in @pyproject.toml
    - format pipeline/ingest.py
    - create github actions workflow for pytest and linting

--- pipeline/ingest.py
+++ pipeline/ingest.py
@@ -1,25 +1,47 @@
 import os
 import sys
-from pathlib import Path
+from pathlib import (
+    Path,
+)
 
 import dotenv
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import (
+    SpotifyOAuth,
+)
 
-from utils.spotify_helpers import get_top_track_uri
+from utils.spotify_helpers import (
+    get_top_track_uri,
+)
 
 dotenv.load_dotenv()
 CLIENT_ID = os.environ.get("client_id")
 CLIENT_SECRET = os.environ.get("client_secret")
 REDIRECT_URI = os.environ.get("redirect_uri")
-PI_SCOPES = "user-modify-playback-state user-library-read user-library-modify playlist-read-private playlist-read-collaborative playlist-modify-public user-top-read user-read-recently-played"
+PI_SCOPES = (
+    "user-modify-playback-state "
+    "user-library-read "
+    "user-library-modify "
+    "playlist-read-private "
+    "playlist-read-collaborative "
+    "playlist-modify-public "
+    "user-top-read "
+    "user-read-recently-played"
+)
 CACHE_PATH = ".spotify_token_cache"
 
 
 def authenticate_user() -> spotipy.Spotify:
-    """Returns spotiy.Spotify instance after authenticating user and saving token to CACHE_PATH"""
+    """Returns spotiy.Spotify instance after authenticating user
+    and saving token to CACHE_PATH"""
 
-    if not all([CLIENT_ID, CLIENT_SECRET, REDIRECT_URI]):
+    if not all(
+        [
+            CLIENT_ID,
+            CLIENT_SECRET,
+            REDIRECT_URI,
+        ]
+    ):
         print("You must have CLIENT_ID, CLIENT_SECRET, AND REDIRECT_URI in .env file")
 
         sys.exit(1)
@@ -41,9 +63,13 @@ def main():
     sp = authenticate_user()
 
     # working test
-    SCRIPT_DIR = Path(__file__).resolve().parent
-    test_path = SCRIPT_DIR.parent / "test" / "test.json"
-    rapsody_uri = get_top_track_uri(sp, "Bohemian rapsody Queen", str(test_path))
+    script_dir = Path(__file__).resolve().parent
+    test_path = script_dir.parent / "test" / "test.json"
+    rapsody_uri = get_top_track_uri(
+        sp,
+        "Bohemian rapsody Queen",
+        str(test_path),
+    )
 
     sp.add_to_queue(rapsody_uri)
 
